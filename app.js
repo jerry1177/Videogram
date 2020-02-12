6f71a1626aca5e41631e0666824895dbc4ff5349
jerry1177 <jerrymaldonadom@gmail.com>
2020-02-12 11:23:05 -0800

    cleaned initial code up

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// MySQL connection section
+
 //const con = mysql.createConnection({ host: "localhost", user: "yourusername", password: "yourpassword"});
+//con.connect(function(err) { if (err) throw err; console.log("Connected!"); });
+
 
 app.get('/',(req, res)=>res.send('Hello World!'));
 app.post('/',(req, res)=>res.send(`username = ${req.body.username} \n password = ${req.body.password}`));
