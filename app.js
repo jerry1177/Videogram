59535f91968e84d2a277d2b2be169329f0bcdf4e
Ubuntu <jerrymaldonadom@gmail.com>
2020-02-25 19:34:09 +0000

    additions to app.js

--- app.js
+++ app.js
@@ -16,12 +16,22 @@ const con = mysql.createConnection({ host: process.env.DB_HOST, user: process.en
 con.connect(function(err) { if (err) throw err; console.log("Connected!"); });
 
 
-app.get('/',(req, res)=>res.send('Hello World!'));
-app.post('/',(req, res)=>res.send(`username = ${req.body.username} \n password = ${req.body.password}`));
+//app.get('/',(req, res)=>res.send('Hello World!'));
+//app.post('/',(req, res)=>res.send(`username = ${req.body.username} \n password = ${req.body.password}`));
+
+app.post('/query', (req, res)=>{
+		var QUERY = req.body.Query;
+		const Connection = require('./MySQLConnection.js')
+		var conn = new Connection.MySQLConnection("videogram");
+		conn.SubmitQuery(QUERY, function(RESULT) {
+			const response = {message:"success", result: RESULT};
+			res.send(JSON.stringify(response));	
+		});
+})
 
 app.post('/user/signup', (req, res)=>{
 	const query = `INSERT INTO Users (Username, Password, Firstname, Lastname, Email) VALUES (${req.body.username}, ${req.body.password}, ${req.body.firstname}, ${req.body.lastname}, ${req.body.email})`;
-
+	console.log("USER Signup Done here");
 	const success = {message:"success"};
 	res.send(JSON.stringify(success));	
 });
