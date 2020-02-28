06100b1af0723d90b909fa391c2c85e7891e4153
Ubuntu <jerrymaldonadom@gmail.com>
2020-02-28 02:39:05 +0000

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+});
 // MySQL connection section
 
 const con = mysql.createConnection({ host: process.env.DB_HOST, user: process.env.DB_USER, password: process.env.DB_PASSWORD});
@@ -23,9 +27,11 @@ var conn = new Connection.MySQLConnection("videogram");
 
 app.post('/query', (req, res)=>{
 		const query = req.body.Query;
+		console.log(req);
 		conn.SubmitQuery(query, function(RESULT) {
 			const response = {message:"success", result: RESULT};
-			res.send(JSON.stringify(response));	
+			res.send(JSON.stringify(response));
+			
 		});
 });
 
