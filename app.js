4c4dfd3b3347346c1d476e961ca2d7dfbb2b4d8d
Derek James <derekjames@q.com>
2020-03-24 20:34:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -85,28 +85,28 @@ app.post('/user/upload/video', (req, res)=>{
 	console.log(req.body);
 	const query = `INSERT INTO Video_Media (Video_Link, User, Upload_Date, Active, Title, Description, Location, Length, Resolution, Fps, Format) VALUES ("${req.body.Video_Link}", "${req.body.User_Id}", "${req.body.Upload_Date}", "${req.body.Active}", "${req.body.Title}", "${req.body.Description}", "${req.body.Location}", "${req.body.Length}", "${req.body.Resolution}", "${req.body.Fps}", "${req.body.Format}")`;
             conn.SubmitQuery(query, function(err, RESULT) {    
-		   /* if (err)
+		   if (err)
                         {
                         	 const response = {message:"failed", reason: "Failed to insert Video_Media"};
-                               res.send(JSON.stringify(response));
+                              res.send(JSON.stringify(response));
 
                         }
                         else
-                        {*/
+                        {
                              const response = {message:"success", result: RESULT};
-				res.send(JSON.stringify(response));
-                        //}
+				             res.send(JSON.stringify(response));
+                        }
         });
 });
 
 app.post('/user/upload/photo', (req, res)=>{
         // make query to video link
         console.log(req.body);
-        const query = `INSERT INTO Photo_Media (Photo_Link) VALUES ("{req.body.link}")`;
+        const query = `INSERT INTO Photo_Media (Photo_Link) VALUES ("{req.body.Photo_Link}")`;
             conn.SubmitQuery(query, function(err, RESULT) {
                     if (err)
                         {
-                                 const response = {message:"failed", reason: "Failed to insert Video_Media"};
+                               const response = {message:"failed", reason: "Failed to insert Photo_Media"};
                                res.send(JSON.stringify(response));
 
                         }
